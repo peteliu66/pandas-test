52113aaa801dc0b1f5775a3fa191795c6816a52b
LIU HAITAO85 <liuhaitao85@crdigital.com>
2022-06-05 14:52:24 +0800

    note-sync syncing

--- data_structures.py
+++ data_structures.py
@@ -24,4 +24,18 @@ df2 = pd.DataFrame(
 )
 print(df2)
 print(df2.dtypes)
-print(df2.shape)
+# Here is a subset of the attributes
+# df2.<TAB>  # noqa: E225, E999
+# df2.A                  df2.bool
+# df2.abs                df2.boxplot
+# df2.add                df2.C
+# df2.add_prefix         df2.clip
+# df2.add_suffix         df2.columns
+# df2.align              df2.copy
+# df2.all                df2.count
+# df2.any                df2.combine
+# df2.append             df2.D
+# df2.apply              df2.describe
+# df2.applymap           df2.diff
+# df2.B                  df2.duplicated
+print(df2.abs)
