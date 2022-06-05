4f015cb3f2dd22290a5b4155980684255a29d38a
LIU HAITAO85 <liuhaitao85@crdigital.com>
2022-06-05 14:39:42 +0800

    note-sync syncing

--- data_structures.py
+++ data_structures.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 # Object creation
 s = pd.Series([1, 3, 5, np.nan, 6, 8])
+print("Object creation")
 print(s)
 # Creating a DataFrame
 dates = pd.date_range("20130101", periods=6)
+print("Creating a DataFrame")
 print(dates)
 
