8658e46fd23e93093cfad59c9ef249fa7465379d
LIU HAITAO85 <liuhaitao85@crdigital.com>
2022-06-05 15:39:50 +0800

    note-sync syncing

--- data_structures.py
+++ data_structures.py
@@ -14,12 +14,12 @@ print(df)
 
 df2 = pd.DataFrame(
     {
-        "A":1.0,
-        "B":pd.Timestamp("20130102"),
-        "C":pd.Series(1,index=list(range(4)), dtype="float32"),
-        "D":np.array([3] * 4, dtype="int32"),
-        "E":pd.Categorical(["test","train","test","train"]),
-        "F":"foo"
+        "A": 1.0,
+        "B": pd.Timestamp("20130102"),
+        "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+        "D": np.array([3] * 4, dtype="int32"),
+        "E": pd.Categorical(["test", "train", "test", "train"]),
+        "F": "foo",
     }
 )
 print(df2)
@@ -59,3 +59,4 @@ print(df.sort_index(axis=1, ascending=False))
 print(df.sort_values(by="B"))
 
 print(df["A"])
+print(df[0:3])
